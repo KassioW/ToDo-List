45b62249c9fcc46c32720038be7d2c0ce5f43bf6
Lukas <lgs20@aluno.ifal.edu.br>
2022-04-19 13:37:59 -0300

    Atualizando

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,7 +6,6 @@ export default function Form() {
   return (
   <Paper style={{ padding: "1em" }}>
     <div style={{ display: "flex", justifyContent: "center" }}>
-
       <TextField id="outlined-basic" label="Tarefa" variant="outlined" fullWidth />
      <Button variant="text">Add</Button>
    </div>
